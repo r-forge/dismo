e516d03891620313a46ebc8f068c79e9a02b4b6d
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2009-12-20 21:15:15 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/georefs/R/gpx.R
@@ -0,0 +1,12 @@
+# Author: Robert J. Hijmans
+# contact: r.hijmans@gmail.com
+# Date : December 2009
+# Version 0.1
+# Licence GPL v3
+
+gpx <- function(file) {
+	doc <- xmlInternalTreeParse(file)
+	
+
+}
+	
