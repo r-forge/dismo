beccc2da2895ecb53002d6dd06724632c1c1bd03
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-23 04:46:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/rmaxent/R/maxent.R
+++ pkg/rmaxent/R/maxent.R
@@ -3,6 +3,11 @@
 maxent <- function(samples, input, output=getwd()) {
 	# create instance of class
 	
+	jar <- paste(system.file(package="rmaxent"), "/java/maxent.jar", sep='')
+	if (!file.exists(jar)) {
+		stop('file missing:', jar, '.\nPlease download it here: http://www.cs.princeton.edu/~schapire/maxent/')
+	}
+	
 	fn <- paste(basename(tempfile()), '.csv', sep='')
 	if (class(samples) == 'data.frame' | class(samples) == 'matrix') {
 		samples <- samples[,1:3]
@@ -11,6 +16,7 @@ maxent <- function(samples, input, output=getwd()) {
 		write.table(samples, file=fn, row.names=FALSE, sep=',')
 		samples <- fn
 	} 
+
 	samples <- samples[1]
 	if (class(samples) != 'character') {
 		stop('invalid samples argument')
@@ -18,16 +24,24 @@ maxent <- function(samples, input, output=getwd()) {
 	if (!file.exists(samples)) {
 		stop('samples file does not exist')
 	}
+	input <- input[1]
 	if (!file.exists(input)) {
 		stop('input folder does not exist')
 	}
+	output <- output[1]
 	if (!file.exists(output)) {
 		stop('output folder does not exist')
 	}
 	
-	
 	mxe <- .jnew("rmaxent") 
-	out <- .jcall(mxe, "S", "invoke", c("autorun", "-e", input, "-o", output, "-s", samples)) 
+	
+	test <- try (  
+		out <- .jcall(mxe, "S", "invoke", c("autorun", "-e", input, "-o", output, "-s", samples)) 
+	)
+	if (class(test) == 'try-error') {
+		out <- 'BOO'
+		warning('something went wrong')
+	}
 	
 	if (file.exists(fn)) file.remove(fn)
 	
