7c94956dc35a5da6711d57a8ebd3a6250d7e3223
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-01-19 03:31:41 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/dismo/R/maxent.predict.R
@@ -0,0 +1,95 @@
+# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# Date: December 2009
+# Version 0.1
+# Licence GPL v3
+
+
+
+if (!isGeneric("predict")) {
+	setGeneric("predict", function(object, ...)
+		standardGeneric("predict"))
+}	
+
+
+setMethod('predict', signature(object='MaxEnt'), 
+	function(object, x, ext=NULL, filename='', progress='text', ...) {
+
+		lambdas <- .maxentTmpFile()
+		
+		write.table(object@lambdas, file=lambdas, row.names=FALSE, col.names=FALSE, quote=FALSE)
+		
+		mxe <- .jnew("mebridge") 
+		filename <- trim(filename)
+		if (inherits(x, "Raster")) {
+			out <- raster(x)
+			if (!is.null(ext)) {
+				ext <- intersectExtent(extent(ext), extent(x))
+				out <- crop(out, ext)
+				firstrow <- rowFromY(x, yFromRow(out, 1))
+				firstcol <- colFromX(x, xFromCol(out, 1))
+				ncols <- colFromX(x, xFromCol(out, ncol(out))) - firstcol + 1
+			} else {
+				firstrow <- 1
+				firstcol <- 1
+				ncols <- ncol(x)
+			}
+			
+			filename <- trim(filename)
+			if (!canProcessInMemory(out, 3) & filename == '') {
+				filename <- rasterTmpFile()
+			}
+			vars <- layerNames(x)
+				# check with model object?
+				
+			if (filename == '') {
+				v <- matrix(ncol=nrow(out), nrow=ncol(out))
+			}
+			pb <- pbCreate(nrow(out), type=progress)
+			cv <- rep(NA, times=ncol(out))
+			for (r in 1:nrow(out)) {
+				rr <- firstrow + r - 1
+				rowvals <- getValuesBlock(x, rr, 1, firstcol, ncols)
+				rowv <- na.omit(rowvals)
+				res <- cv
+				if (length(rowv) > 0) {
+					p <- .jcall(mxe, "[D", "predict", lambdas, vars, .jarray(rowv)) 
+					naind <- as.vector(attr(rowv, "na.action"))
+					if (!is.null(naind)) {
+						res[-naind] <- p
+					} else {
+						res <- p
+					}
+				}
+				if (filename != '') {
+					out <- setValues(out, res, r)
+					out <- writeRaster(out, filename=filename, ...)
+				} else {
+					v[,r] <- res
+				}
+				pbStep(pb, r) 
+			} 
+			pbClose(pb)
+			if (filename  == '') {
+				out <- setValues(out, as.vector(v))
+			}
+		} else {
+			if (inherits(x, "Spatial")) {
+				x <- as.data.frame(x)
+			}
+			vars <- colnames(x)
+			rowv <- na.omit(x)
+			out <- rep(NA, times=nrow(x))
+			if (length(rowv) > 0) {
+				p <- .jcall(mxe, "[D", "predict", lambdas, vars, .jarray(rowv)) 
+				naind <- as.vector(attr(rowv, "na.action"))
+				if (!is.null(naind)) {
+					out[-naind] <- p
+				} else {
+					out <- p
+				}
+			} 
+		}
+		#try( file.remove(lambdas), silent=TRUE )
+		out
+	}
+)
