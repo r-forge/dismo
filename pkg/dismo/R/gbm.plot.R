b3ae78693f3266f6663e89e6c665ec440077898d
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-02-04 16:32:40 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@96 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/gbm.plot.R
+++ pkg/dismo/R/gbm.plot.R
@@ -1,96 +1,12 @@
-
-gbm.plot.fits <- function(gbm.object, 
-  mask.presence = FALSE, 
-  use.factor = FALSE,
-  plot.layout = c(3,4)          # define the default layout for graphs on the page
-)
-{
+# function to plot gbm response variables, with the option
+# of adding a smooth representation of the response if requested
+# additional options in this version allow for plotting on a common scale
+# note too that fitted functions are now centered by subtracting their mean
+# 
+# version 2.9
 #
-# j leathwick, j elith - 7th January 2005
+# j. leathwick/j. elith - March 2007
 #
-# version 2.0 - developed in R 2.0
-#
-# to plot distribution of fitted values in relation to ydat from mars or other p/a models
-# allows masking out of absences to enable focus on sites with high predicted values
-# fitted values = those from model; raw.values = original y values
-# label = text species name; ydat = predictor dataset
-# mask.presence forces function to only plot fitted values for presences
-# use.factor forces to use quicker printing box and whisker plot
-# file.name routes to a pdf file of this name
-#
-
-	max.plots <- plot.layout[1] * plot.layout[2]
-    plot.count <- 0
-
-    dat <- gbm.object$gbm.call$dataframe    #get the dataframe name
-    dat <- as.data.frame(eval(parse(text=dat)))   #and now the data
-
-    n.cases <- nrow(dat)
-
-    gbm.call <- gbm.object$gbm.call	#and the mars call details
-    gbm.x <- gbm.call$gbm.x    
-    gbm.y <- gbm.call$gbm.y
-    family <- gbm.call$family
-
-    xdat <- as.data.frame(dat[,gbm.x])
-    ydat <- as.data.frame(dat[,gbm.y])
-
-    n.preds <- ncol(xdat)
-
-    fitted.values <- gbm.object$fitted
-
-    pred.names <- names(dat)[gbm.x]
-    sp.name <- names(dat)[gbm.y]
-
-    if (mask.presence) {
-		mask <- ydat == 1 
-	} else {
-	  mask <- rep(TRUE, length = n.cases) 
-	}
-
-    robust.max.fit <- approx(ppoints(fitted.values[mask]), sort(fitted.values[mask]), 0.99) #find 99%ile value
-
-    for (j in 1:n.preds) {
-
-		if (plot.count == max.plots) {
-			plot.count = 0
-		}
-
-		if (plot.count == 0) {
-			par(mfrow = plot.layout)
-		}
-
-		plot.count <- plot.count + 1
-	
-		if (is.numeric(xdat[mask,j])) {
-			wt.mean <- zapsmall(mean((xdat[mask, j] * fitted.values[mask]^5)/mean(fitted.values[mask]^5),na.rm=TRUE),2)
-		} else {
-			wt.mean <- "na"
-		}
-		
-		if (use.factor) {
-			temp <- factor(cut(xdat[mask, j], breaks = 12))
-			if (family == "binomial") {
-				plot(temp, fitted.values[mask], xlab = pred.names[j], ylab = "fitted values", ylim = c(0, 1))
-			} else {
-				plot(temp, fitted.values[mask], xlab = pred.names[j], ylab = "fitted values")
-			}
-		} else {
-			if (family == "binomial") {
-				plot(xdat[mask, j], fitted.values[mask], xlab = pred.names[j], ylab = "fitted values", ylim = c(0, 1))
-			} else {
-				plot(xdat[mask, j], fitted.values[mask], xlab = pred.names[j], ylab = "fitted values")}
-		    }
-			abline(h = (0.333 * robust.max.fit$y), lty = 2.)
-			if (j == 1) { 
-				title(paste(sp.name, ", wtm = ", wt.mean))
-			} else {
-				title(paste("wtm = ", wt.mean))}
-		}
-}
-
-
-
 
 
 gbm.plot <-
@@ -109,15 +25,6 @@ function(gbm.object,                # a gbm object - could be one from gbm.step
                                     # useful options include cex.axis, cex.lab, etc.
      )
 {
-# function to plot gbm response variables, with the option
-# of adding a smooth representation of the response if requested
-# additional options in this version allow for plotting on a common scale
-# note too that fitted functions are now centered by subtracting their mean
-# 
-# version 2.9
-#
-# j. leathwick/j. elith - March 2007
-#
 
   if (! require(gbm) ) { stop ('you need to install the gbm package to run this function') }
   if (! require(splines) ) { stop ('you need to install the gbm package to run this function') }
