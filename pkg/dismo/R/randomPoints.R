be2f91baeb927f0d206d7a1c7460d282ca4d1982
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-02-07 20:29:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/randomPoints.R
+++ pkg/dismo/R/randomPoints.R
@@ -55,7 +55,7 @@ randomPoints <- function(mask, n, p, ext=NULL, extf=1.1, excludep=TRUE, tryf=5, 
 	if (nn == ncell(mask2)) {
 		cells <- 1:ncell(mask2)
 	} else {
-		cells <- unique(round(runif(nn) * ncell(mask2)))
+		cells <- sample.int(ncell(mask2), nn)
 	}
 	xy <- xyFromCell(mask2, cells)
 	cells <- cellFromXY(mask, xy)
