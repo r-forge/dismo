e49e09bbbd6676c7035897c01b2488ecef3dccbe
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2010-07-18 22:41:00 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@192 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/inst/javasrc/mebridge.java
+++ pkg/dismo/inst/javasrc/mebridge.java
@@ -36,7 +36,7 @@ public class mebridge {
 
 			eval = new Evaluate(lambda);
 			Params params = eval.getParams();	
-			//params.readFromArgs(cmd);
+			params.readFromArgs(args);
 		
 		} catch (java.io.IOException e) {}
 
@@ -54,36 +54,36 @@ public class mebridge {
 		return(p);
 	}
 	
-	public double predict (String lambda, String[] vars, int[] vals, String[] args) {
-		Evaluate eval=null;
-		try {
-			eval = new Evaluate(lambda);
-		} catch (java.io.IOException e) {}
-		for (int i=0; i<vars.length; i++)
-			eval.setValue(vars[i], vals[i]);
-		return( eval.evaluate() );
-	}
+//	public double predict (String lambda, String[] vars, int[] vals, String[] args) {
+//		Evaluate eval=null;
+//		try {
+//			eval = new Evaluate(lambda);
+//		} catch (java.io.IOException e) {}
+//		for (int i=0; i<vars.length; i++)
+//			eval.setValue(vars[i], vals[i]);
+//		return( eval.evaluate() );
+//	}
 	
 
 
-	public double[] predict (String lambda, String[] vars, int[][] vals, String[] args) {
-		Evaluate eval=null;
-		try {
-			eval = new Evaluate(lambda);
-		} catch (java.io.IOException e) {}
-		double[] p = new double[vals.length];
-		for (int i=0; i<p.length; i++) {
-			for (int j=0; j<vars.length; j++) {
-				eval.setValue(vars[j], vals[i][j]);
-				try {
-					p[i] = eval.evaluate();
-				} catch (Exception e) {
-					p[i] = -9999;
-				}
-			}
-		}
-		return(p);
-	}
+//	public double[] predict (String lambda, String[] vars, int[][] vals, String[] args) {
+//		Evaluate eval=null;
+//		try {
+//			eval = new Evaluate(lambda);
+//		} catch (java.io.IOException e) {}
+//		double[] p = new double[vals.length];
+//		for (int i=0; i<p.length; i++) {
+//			for (int j=0; j<vars.length; j++) {
+//				eval.setValue(vars[j], vals[i][j]);
+//				try {
+//					p[i] = eval.evaluate();
+//				} catch (Exception e) {
+//					p[i] = -9999;
+//				}
+//			}
+//		}
+//		return(p);
+//	}
 
 	
 	public void fit(String cmd[]) {
