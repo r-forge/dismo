ec1c285992102707626c07160811f703c1973f62
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-03 04:27:11 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@282 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/biovars.R
+++ pkg/dismo/R/biovars.R
@@ -49,7 +49,7 @@ if (!isGeneric("biovars")) {
 
 
 setMethod('biovars', signature(prec='vector', tmin='vector', tmax='vector'), 
-	function(prec, tmin, tmax, filename='', ...) {
+	function(prec, tmin, tmax) {
 		biovars(t(as.matrix(prec)), t(as.matrix(tmin)), t(as.matrix(tmax)))
 	}
 )
@@ -83,14 +83,16 @@ setMethod('biovars', signature(prec='Raster', tmin='Raster', tmax='Raster'),
 	tr <- blockSize(out, n=nlayers(out)+36)
 	pb <- pbCreate(tr$n, type=progress)	
 	for (i in 1:tr$n) {
-		prc <- getValues(prec, tr$row[i])
-		tmn <- getValues(tmin, tr$row[i])
-		tmx <- getValues(tmax, tr$row[i])
-		p <- biovars(prc, tmn, tmx, ...)
+		prc <- getValues(prec, tr$row[i], tr$nrows[i])
+		tmn <- getValues(tmin, tr$row[i], tr$nrows[i])
+		tmx <- getValues(tmax, tr$row[i], tr$nrows[i])
+		p <- biovars(prc, tmn, tmx)
 		if (filename != "") {
 			out <- writeValues(out, p, tr$row[i])
 		} else {
-			v[tr$row[i]:(tr$row[i]+tr$nrows[i]-1),] <- p
+			start <- (tr$row[i]-1) * out@ncols + 1
+			end <- (tr$row[i]+tr$nrows[i]-1) * out@ncols
+			v[start:end,] <- p
 		}
 	}
 	
@@ -107,7 +109,7 @@ setMethod('biovars', signature(prec='Raster', tmin='Raster', tmax='Raster'),
 
 
 setMethod('biovars', signature(prec='matrix', tmin='matrix', tmax='matrix'), 
-	function(prec, tmin, tmax, filename='', ...) {
+	function(prec, tmin, tmax) {
 
 		if (nrow(prec) != nrow(tmin) | nrow(tmin) != nrow(tmax) ) {
 			stop('prec, tmin and tmax should have same length')
