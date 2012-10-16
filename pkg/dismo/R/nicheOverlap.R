4f1fc9941e8495cc5faaf945a96a5fa01dde1c66
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-10-16 17:52:05 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@434 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/nicheOverlap.R
+++ pkg/dismo/R/nicheOverlap.R
@@ -5,19 +5,23 @@
 # Licence GPL v3
 
 
-nicheOverlap <- function (x, y) {
+nicheOverlap <- function (x, y, mask=TRUE, checkNegatives=TRUE) {
 	s <- stack(x, y)
 
 	# to assure that both have the same NA cells
-	s <- mask(s,  x * y)
-	
-	minv <- cellStats(s, 'min', na.rm=TRUE)
-	if (any(minv < 0)) {
-		stop('values of "x" and "y" should be non-negative')
+	if (mask) {
+		s <- mask(s,  x * y)
 	}
 	
-    s <- s / cellStats(s, 'sum', na.rm=TRUE)
-	r <- overlay(s, fun=function(i,j) (sqrt(i) - sqrt(j))^2)
+	if (checkNegatives) {
+		minv <- cellStats(s, 'min', na.rm=TRUE)
+		if (any(minv < 0)) {
+			stop('values of "x" and "y" should be non-negative')
+		}
+	}
+	
+    cs <- cellStats(s, 'sum', na.rm=TRUE)
+	r <- overlay(s, fun=function(i,j) (sqrt(i / cs[1]) - sqrt(j / cs[2] ))^2)
 	1 - 0.5 * sqrt(cellStats(r, 'sum', na.rm=TRUE))
 }
 
