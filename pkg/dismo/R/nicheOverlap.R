dc8b516d0b37020c7248419ba78f28fbf8e7f166
rhijmans <rhijmans@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-16 17:35:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@433 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/nicheOverlap.R
+++ pkg/dismo/R/nicheOverlap.R
@@ -1,17 +1,23 @@
-# Author: Robert J. Hijmans
 # based on function Istat in package SDMTools by Jeremy VanDerWal
+# adapted by Robert J. Hijmans 
 # Date : October 2012
 # Version 1.0
 # Licence GPL v3
 
 
 nicheOverlap <- function (x, y) {
-	compareRaster(x,y)
-	stopifnot(cellStats(x, 'min') >= 0)
-	stopifnot(cellStats(y, 'min') >= 0)
-    rx <- x/cellStats(x, 'sum')
-    ry <- y/cellStats(y, 'sum')
-	r <- overlay(rx, ry, fun=function(i,j) (sqrt(i) - sqrt(j))^2)
-	1 - 0.5 * sqrt(cellStats(r, 'sum'))
+	s <- stack(x, y)
+
+	# to assure that both have the same NA cells
+	s <- mask(s,  x * y)
+	
+	minv <- cellStats(s, 'min', na.rm=TRUE)
+	if (any(minv < 0)) {
+		stop('values of "x" and "y" should be non-negative')
+	}
+	
+    s <- s / cellStats(s, 'sum', na.rm=TRUE)
+	r <- overlay(s, fun=function(i,j) (sqrt(i) - sqrt(j))^2)
+	1 - 0.5 * sqrt(cellStats(r, 'sum', na.rm=TRUE))
 }
 
