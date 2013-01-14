e15c69772a5fb30f55a8ab057b4e4af382ab5701
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2013-01-14 18:32:51 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@466 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/geocode.R
+++ pkg/dismo/R/geocode.R
@@ -8,64 +8,70 @@ geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='') {
 	if (! require(XML)) stop('You need to install the XML package to be able use this function')
 
 	burl <- "http://maps.google.com/maps/api/geocode/xml?address="
-
+	
 
 	res <- matrix(ncol=7, nrow=0)
 	colnames(res) <- c('ID', 'lon', 'lat', 'lonmin', 'lonmax', 'latmin', 'latmax')
-	
 	pb <- pbCreate(length(x), progress)
 	for (z in 1:length(x)) {
 		r <- x[z]
 		r <- gsub(', ', ',', r)
 		r <- gsub(' ,', ',', r)
-		r <- gsub(' ', '+', r)
-		if (is.null(extent)) {
-			gurl <- paste(burl, r, "&sensor=false", sep="")
-		} else {
-			e <- extent(extent)
-			extent <- paste(e@ymin,',',e@xmin,'|',e@ymax,',',e@xmax,sep='')
-			gurl <- paste(burl, r, "&bounds=", extent, "&sensor=false", sep="")			
-		}
-		try( doc <- xmlInternalTreeParse(gurl) )
-		if (class(doc)[1] == 'try-error') {
-			warning('cannot parse XML document\n')
-			status <- ''
-		} else { 
-			status <- xmlValue(getNodeSet(doc, "//GeocodeResponse//status")[[1]])
-		}
+		r <- trim(r)
+		if (length(r) > 0 & !is.na(r)) {
+			r <- gsub(' ', '+', r)
+			if (is.null(extent)) {
+				gurl <- paste(burl, r, "&sensor=false", sep="")
+			} else {
+				e <- extent(extent)
+				extent <- paste(e@ymin,',',e@xmin,'|',e@ymax,',',e@xmax,sep='')
+				gurl <- paste(burl, r, "&bounds=", extent, "&sensor=false", sep="")			
+			}
+			try( doc <- xmlInternalTreeParse(gurl) )
+			if (class(doc)[1] == 'try-error') {
+				warning('cannot parse XML document\n')
+				status <- ''
+			} else { 
+				status <- xmlValue(getNodeSet(doc, "//GeocodeResponse//status")[[1]])
+			}
+			
+			if (status != "OK") {
+				cat(status, ':', r, '\n')
+				w <- matrix(NA, ncol=ncol(res), nrow=1)
+				w[1] <- z
+				res <- rbind(res, w)
+				next
+			}
 		
-		if (status != "OK") {
-			cat(status, ':', r, '\n')
+			p <- xmlToList(doc)
+			n <- length(p)-1
+			location <- matrix(ncol=2, nrow=n)
+			viewport <- matrix(ncol=4, nrow=n)
+			bounds <- matrix(ncol=4, nrow=n)
+			for (i in 1:n) {
+				location[i,] <- as.numeric(c(p[i+1]$result$geometry$location$lng, p[i+1]$result$geometry$location$lat))
+				viewport[i,] <- as.numeric(c(p[i+1]$result$geometry$viewport$southwest$lng, p[i+1]$result$geometry$viewport$northeast$lng, p[i+1]$result$geometry$viewport$southwest$lat, p[i+1]$result$geometry$viewport$northeast$lat) )
+				bnds <- as.numeric(c(p[i+1]$result$geometry$bounds$southwest$lng, p[i+1]$result$geometry$bounds$northeast$lng, p[i+1]$result$geometry$bounds$southwest$lat, p[i+1]$result$geometry$bounds$northeast$lat) )
+				if (length(bnds)==4) bounds[i,] <- bnds
+			}
+
+			w <- cbind(viewport, bounds)
+			w[,c(1,3)] <- pmin(w[,c(1,3)], w[,c(1,3)+4], na.rm=TRUE)
+			w[,c(2,4)] <- pmax(w[,c(2,4)], w[,c(2,4)+4], na.rm=TRUE)
+			w <- cbind(z, location, w[,1:4,drop=FALSE])
+		
+			if (oneRecord & nrow(w) > 1) {
+				f <- apply(w[,4:7, drop=FALSE], 2, range)
+				g <- apply(w[,2:3, drop=FALSE], 2, mean)
+				w <- cbind(z, g[1], g[2], f[1,1], f[2,2], f[1,3], f[2,4])
+			}
+
+			res <- rbind(res, w)
+		} else {
 			w <- matrix(NA, ncol=ncol(res), nrow=1)
 			w[1] <- z
 			res <- rbind(res, w)
-			next
 		}
-		
-		p <- xmlToList(doc)
-		n <- length(p)-1
-		location <- matrix(ncol=2, nrow=n)
-		viewport <- matrix(ncol=4, nrow=n)
-		bounds <- matrix(ncol=4, nrow=n)
-		for (i in 1:n) {
-			location[i,] <- as.numeric(c(p[i+1]$result$geometry$location$lng, p[i+1]$result$geometry$location$lat))
-			viewport[i,] <- as.numeric(c(p[i+1]$result$geometry$viewport$southwest$lng, p[i+1]$result$geometry$viewport$northeast$lng, p[i+1]$result$geometry$viewport$southwest$lat, p[i+1]$result$geometry$viewport$northeast$lat) )
-			bnds <- as.numeric(c(p[i+1]$result$geometry$bounds$southwest$lng, p[i+1]$result$geometry$bounds$northeast$lng, p[i+1]$result$geometry$bounds$southwest$lat, p[i+1]$result$geometry$bounds$northeast$lat) )
-			if (length(bnds)==4) bounds[i,] <- bnds
-		}
-
-		w <- cbind(viewport, bounds)
-		w[,c(1,3)] <- pmin(w[,c(1,3)], w[,c(1,3)+4], na.rm=TRUE)
-		w[,c(2,4)] <- pmax(w[,c(2,4)], w[,c(2,4)+4], na.rm=TRUE)
-		w <- cbind(z, location, w[,1:4,drop=FALSE])
-	
-		if (oneRecord & nrow(w) > 1) {
-			f <- apply(w[,4:7, drop=FALSE], 2, range)
-			g <- apply(w[,2:3, drop=FALSE], 2, mean)
-			w <- cbind(z, g[1], g[2], f[1,1], f[2,2], f[1,3], f[2,4])
-		}
-
-		res <- rbind(res, w)
 		pbStep(pb, z) 
 	} 
 	pbClose(pb)
