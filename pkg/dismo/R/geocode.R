a8a71b43987e467256f53700b8c4df4da8b2fdce
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2013-01-16 03:52:43 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@470 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/geocode.R
+++ pkg/dismo/R/geocode.R
@@ -1,17 +1,20 @@
-# Author: Robert J. Hijmans, r.hijmans@gmail.com
+# Author: Robert J. Hijmans
 # License GPL3
 # Version 1.0
 # October 2010
 
-geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='') {
+geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='', ...) {
+
+	if (!is.null(list(...)$oneRecords)) {
+		warning('argument "oneRecord" is no longer used')
+	}
 
 	if (! require(XML)) stop('You need to install the XML package to be able use this function')
 
 	burl <- "http://maps.google.com/maps/api/geocode/xml?address="
 	
 
-	res <- matrix(ncol=7, nrow=0)
-	colnames(res) <- c('ID', 'lon', 'lat', 'lonmin', 'lonmax', 'latmin', 'latmax')
+	res <- data.frame(matrix(ncol=8, nrow=0))
 	pb <- pbCreate(length(x), progress)
 	for (z in 1:length(x)) {
 		r <- x[z]
@@ -27,7 +30,7 @@ geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='') {
 				extent <- paste(e@ymin,',',e@xmin,'|',e@ymax,',',e@xmax,sep='')
 				gurl <- paste(burl, r, "&bounds=", extent, "&sensor=false", sep="")			
 			}
-			try( doc <- xmlInternalTreeParse(gurl) )
+			try( doc <- xmlInternalTreeParse(gurl, isURL=TRUE) )
 			if (class(doc)[1] == 'try-error') {
 				warning('cannot parse XML document\n')
 				status <- ''
@@ -45,10 +48,12 @@ geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='') {
 		
 			p <- xmlToList(doc)
 			n <- length(p)-1
+			place <- rep(NA, n)
 			location <- matrix(ncol=2, nrow=n)
 			viewport <- matrix(ncol=4, nrow=n)
 			bounds <- matrix(ncol=4, nrow=n)
 			for (i in 1:n) {
+				place[i] <- p[i+1]$result$formatted_address
 				location[i,] <- as.numeric(c(p[i+1]$result$geometry$location$lng, p[i+1]$result$geometry$location$lat))
 				viewport[i,] <- as.numeric(c(p[i+1]$result$geometry$viewport$southwest$lng, p[i+1]$result$geometry$viewport$northeast$lng, p[i+1]$result$geometry$viewport$southwest$lat, p[i+1]$result$geometry$viewport$northeast$lat) )
 				bnds <- as.numeric(c(p[i+1]$result$geometry$bounds$southwest$lng, p[i+1]$result$geometry$bounds$northeast$lng, p[i+1]$result$geometry$bounds$southwest$lat, p[i+1]$result$geometry$bounds$northeast$lat) )
@@ -58,12 +63,13 @@ geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='') {
 			w <- cbind(viewport, bounds)
 			w[,c(1,3)] <- pmin(w[,c(1,3)], w[,c(1,3)+4], na.rm=TRUE)
 			w[,c(2,4)] <- pmax(w[,c(2,4)], w[,c(2,4)+4], na.rm=TRUE)
-			w <- cbind(z, location, w[,1:4,drop=FALSE])
-		
+			w <- w[,1:4,drop=FALSE]
 			if (oneRecord & nrow(w) > 1) {
-				f <- apply(w[,4:7, drop=FALSE], 2, range)
-				g <- apply(w[,2:3, drop=FALSE], 2, mean)
-				w <- cbind(z, g[1], g[2], f[1,1], f[2,2], f[1,3], f[2,4])
+				f <- apply(w, 2, range)
+				g <- apply(location, 2, mean)
+				w <- data.frame(z, NA, g[1], g[2], f[1,1], f[2,2], f[1,3], f[2,4])
+			} else {
+				w <- data.frame(z, place, location, w)
 			}
 
 			res <- rbind(res, w)
@@ -76,10 +82,26 @@ geocode <- function(x, oneRecord=FALSE, extent=NULL, progress='') {
 	} 
 	pbClose(pb)
 
+	colnames(res) <- c('ID', 'formatted_locality', 'lon', 'lat', 'lonmin', 'lonmax', 'latmin', 'latmax')
 	rownames(res) <- 1:nrow(res)
-	x <- data.frame(ID=1:length(x), location=x)
-	res <- merge(res, x, by='ID')
-	return(res)	
+	
+	da <- pointDistance(res[,3:4], res[,c(5,7)], longlat=T)
+	db <- pointDistance(res[,3:4], res[,c(6,8)], longlat=T)
+	res$uncertainty <- round(pmin(da, db))
+	xx <- data.frame(ID=1:length(x), original_locality=x)
+	
+	merge(res, xx, by='ID')
 }
 
 
+#a = geocode('San Jose, Mexico', oneRecord=F)
+
+
+
+
+
+
+ a = geocode('San Jose, Mexico', oneRecord=T)
+
+ 
+ 
