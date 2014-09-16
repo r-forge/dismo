63d4a739e7614316de3708d9fd1f8861fb0ac14c
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2014-09-16 06:11:28 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dismo@551 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/dismo/R/rowMinMax.R
+++ pkg/dismo/R/rowMinMax.R
@@ -3,22 +3,29 @@
 
 .rowMin <- function(x, na.rm=TRUE) {
    # Construct a call pmin(x[,1],x[,2],...x[,NCOL(x)])
-    code <- paste("x[,",1:(NCOL(x)),"]",sep="",collapse=",")
-    code <- paste("pmin.int(",code,", na.rm =",na.rm,")")
-    return(eval(parse(text=code)))
+    #code <- paste("x[,",1:(NCOL(x)),"]",sep="",collapse=",")
+    #code <- paste("pmin.int(",code,", na.rm =",na.rm,")")
+    #return(eval(parse(text=code)))
+	apply(x, 1, min, na.rm=na.rm)
 }
 
 .rowMax <- function(x, na.rm=TRUE) {
-    code <- paste("x[,",1:(NCOL(x)),"]",sep="",collapse=",")
-    code <- paste("pmax.int(",code,", na.rm =",na.rm,")")
-    return(eval(parse(text=code)))
+#    code <- paste("x[,",1:(NCOL(x)),"]",sep="",collapse=",")
+#    code <- paste("pmax.int(",code,", na.rm =",na.rm,")")
+#    return(eval(parse(text=code)))
+	apply(x, 1, max, na.rm=na.rm)
+	
 }
 
 .colMin <- function(x, na.rm=TRUE) {
-	.rowMin(t(x), na.rm=na.rm)
+#	.rowMin(t(x), na.rm=na.rm)
+	apply(x, 2, min, na.rm=na.rm)
+
 }
 
 .colMax <- function(x, na.rm=TRUE) {
-	.rowMax(t(x), na.rm=na.rm)
+#	.rowMax(t(x), na.rm=na.rm)
+	apply(x, 2, max, na.rm=na.rm)
+
 }
 
